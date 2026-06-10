249fc07c7c9a2c129105a6cdd2db1a3f47b3d7d9
amitkumar-vishva <amitvishvait0007@gmail.com>
2026-06-10 12:25:48 +0530

    java questions

---
+++ javatopics/if-else01/ifelse.java
@@ -0,0 +1,197 @@
+// ---------------------------------------------------------------------------
+//         check given number is even number 
+// ---------------------------------------------------------------------------
+// import java.util.*;
+// public class ifelse {
+//     public static void main(String[] args) {
+//         int n;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the number : ");
+//         n = ob.nextInt();
+
+//         if(n%2==0){
+//             System.out.print("Given number is even number ");
+//         }
+//         else{
+//             System.out.print("Given number is odd number ");
+//         }
+//     }
+// }
+
+
+// ---------------------------------------------------------------------------
+//         check given number is positive and negitive number 
+// ---------------------------------------------------------------------------
+// import java.util.*;
+// public class ifelse {
+//     public static void main(String[] args) {
+//         int n;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the number : ");
+//         n = ob.nextInt();
+
+//         if(n>0){
+//             System.out.print("Given number is positive");
+//         }
+//         else{
+//             System.out.print("Given number is negitive ");
+//         }
+//     }
+// }
+
+
+// ---------------------------------------------------------------------------
+//         check voting system 
+// ---------------------------------------------------------------------------
+// import java.util.*;
+// public class ifelse {
+//     public static void main(String[] args) {
+//         int n;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the number : ");
+//         n = ob.nextInt();
+
+//         if(n>18){
+//             System.out.print("You are Eligible for voting ");
+//         }
+//         else{
+//             System.out.print("You are not Eligible for voting");
+//         }
+//     }
+// }
+
+
+// ---------------------------------------------------------------------------
+//         You find the two number which number is greater 
+// ---------------------------------------------------------------------------
+// import java.util.*;
+// public class ifelse {
+//     public static void main(String[] args) {
+//         int num1,num2;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the first number : ");
+//         num1 = ob.nextInt();
+
+//         System.out.print("Enter the second number : ");
+//         num2 = ob.nextInt();
+
+//         if(num1>num2){
+//             System.out.print("number 01 greater then number 02 se : " +num1);
+//         }
+//         else{
+//             System.out.print("number 02 greater then number 01 se : " +num2);
+//         }
+//     }
+// }
+
+
+// ---------------------------------------------------------------------------
+//         check given number is even number 
+// ---------------------------------------------------------------------------
+// import java.util.*;
+// public class ifelse {
+//     public static void main(String[] args) {
+//         int num1;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the first number : ");
+//         num1 = ob.nextInt();
+
+//         if(num1>=90){
+//             System.out.print("A is greater : " +num1);
+//         }
+//         else if (num1>=75 && num1<=89) {
+//             System.out.print("B is greater : " +num1);
+//         }
+//         else if (num1>=60 && num1<=75) {
+//             System.out.print("C is greater : " +num1);
+//         }
+//         else if (num1>=40 && num1<=59) {
+//             System.out.print("D is greater : " +num1);
+//         }
+//         else{
+//             System.out.print("Fails");
+//         }
+//     }
+// }
+
+
+// ---------------------------------------------------------------------------
+//         check given year is leap year 
+// ---------------------------------------------------------------------------
+// import java.util.*;
+// public class ifelse {
+//     public static void main(String[] args) {
+//         int y;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the leap yare : ");
+//         y = ob.nextInt();
+
+//         if(y%4==0){
+//             System.out.print("Given year is leap year ");
+//         }
+//         else{
+//             System.out.print("Given year is not leap year ");
+//         }
+//     }
+// }
+
+
+// ---------------------------------------------------------------------------
+//         check Temperature
+// ---------------------------------------------------------------------------
+
+// import java.util.*;
+// public class ifelse {
+//     public static void main(String[] args) {
+//         int num1;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the first number : ");
+//         num1 = ob.nextInt();
+
+//         if(num1>=40){
+//             System.out.print("Very hot ");
+//         }
+//         else if (num1>=30 && num1<=39) {
+//             System.out.print("Hot  ");
+//         }
+//         else if (num1>=20 && num1<=29) {
+//             System.out.print("Normal ");
+//         }
+//         else{
+//             System.out.print("Cold");
+//         }
+//     }
+// }
+
+
+// ---------------------------------------------------------------------------
+//         find largest number in three number 
+// ---------------------------------------------------------------------------
+
+// import java.util.*;
+// public class ifelse {
+//     public static void main(String[] args) {
+//         int num1,num2,num3;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the first number : ");
+//         num1 = ob.nextInt();
+
+//         System.out.print("Enter the second number : ");
+//         num2 = ob.nextInt();
+
+//         System.out.print("Enter the third number : ");
+//         num3 = ob.nextInt();
+
+//         if(num1>num2 && num1>num3){
+//             System.out.print("Number 01 greater then num1 and num2");
+//         }
+//         else if (num2>num1 && num2>num3) {
+//             System.out.print("Number 02 greater then num1 and num1");
+//         }
+//         else{
+//             System.out.print("Number 03 greater then num1 and num2");
+//         }
+//     }
+// }
+
+
