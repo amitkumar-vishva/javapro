68650f1eb18acf9865d26b08537c6b9b73322328
amitkumar-vishva <amitvishvait0007@gmail.com>
2026-06-11 16:01:13 +0530

    adds more programs

--- javatopics/loops02/loops.java
+++ javatopics/loops02/loops.java
@@ -1,5 +1,303 @@
-package topics.javatopics.loops02;
+// ---------------------------------------------------------------
+//     1 se N number ki ginti print karo
+// ---------------------------------------------------------------
+// import java.util.*;
+// public class loops {
+//     public static void main(String[] args) {
+//         int n,i;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the number : ");
+//         n = ob.nextInt();
 
-public class loops {
-    
-}
+//         for(i=1;i<=n;i++){
+//             System.out.print(i +" ");
+//         }
+//     }
+// }
+
+// ---------------------------------------------------------------
+//     n se 1 tak ki ulti ginti print karo
+// ---------------------------------------------------------------
+// import java.util.*;
+// public class loops {
+//     public static void main(String[] args) {
+//         int n,i;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the number : ");
+//         n = ob.nextInt();
+
+//         for(i=n;i>=1;i--){
+//             System.out.print(i +" ");
+//         }
+//     }
+// }
+
+// ---------------------------------------------------------------
+//     1 se N tak ke sabhi even number print karo
+// ---------------------------------------------------------------
+// import java.util.*;
+// public class loops {
+//     public static void main(String[] args) {
+//         int n,i;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the number : ");
+//         n = ob.nextInt();
+
+//         for(i=1;i<=n;i++){
+//             if(i%2==0){
+//                 System.out.print(i +" ");
+//             }
+//         }
+//     }
+// }
+
+
+// ---------------------------------------------------------------
+//     1 se N tak ke sabhi odd number print karo
+// ---------------------------------------------------------------
+// import java.util.*;
+// public class loops {
+//     public static void main(String[] args) {
+//         int n,i;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the number : ");
+//         n = ob.nextInt();
+
+//         for(i=1;i<=n;i++){
+//             if(i%2!=0){
+//                 System.out.print(i +" ");
+//             }
+//         }
+//     }
+// }
+
+
+// ---------------------------------------------------------------
+//     1 se N tak ke sabhi no ka sum karo
+// ---------------------------------------------------------------
+// import java.util.*;
+// public class loops {
+//     public static void main(String[] args) {
+//         int n,i,sum=0;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the number : ");
+//         n = ob.nextInt();
+
+//         for(i=1;i<=n;i++){
+//             sum = sum + i;
+//         }
+//         System.out.print("Sum of given number : " +sum);
+//     }
+// }
+
+// ---------------------------------------------------------------
+//     1 se N tak ke sabhi even number ko add karo
+// ---------------------------------------------------------------
+// import java.util.*;
+// public class loops {
+//     public static void main(String[] args) {
+//         int n,i,sum=0;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the number : ");
+//         n = ob.nextInt();
+
+//         for(i=1;i<=n;i++){
+//             if(i%2==0){
+//                 sum = sum + i;
+//             }
+//         }
+//         System.out.print("Sum of given even number : "+sum);
+//     }
+// }
+
+// ---------------------------------------------------------------
+//     1 se N tak ke sabhi odd number print karo
+// ---------------------------------------------------------------
+// import java.util.*;
+// public class loops {
+//     public static void main(String[] args) {
+//         int n,i,sum=0;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the number : ");
+//         n = ob.nextInt();
+
+//         for(i=1;i<=n;i++){
+//             if(i%2!=0){
+//                 sum = sum + i;
+//             }
+//         }
+//         System.out.print("Sum of odd number : " +sum);
+//     }
+// }
+
+// ---------------------------------------------------------------
+//     n number ka table print karo
+// ---------------------------------------------------------------
+// import java.util.*;
+// public class loops {
+//     public static void main(String[] args) {
+//         int n,i;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the number : ");
+//         n = ob.nextInt();
+
+//         for(i=1;i<=10;i++){
+//             System.out.println(n +" * " +i +" = " +n*i);
+//         }
+//     }
+// }
+
+// ---------------------------------------------------------------
+//     factorial nikalo kis bhi number ka 
+// ---------------------------------------------------------------
+// import java.util.*;
+// public class loops {
+//     public static void main(String[] args) {
+//         int n,i,fact=1;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the number : ");
+//         n = ob.nextInt();
+
+//         for(i=1;i<=n;i++){
+//             fact = fact * i;
+//         }
+//         System.out.println("Factorial of given number : " +fact);
+//     }
+// }
+
+// ---------------------------------------------------------------
+//     kisi bhi no ka squares print karo
+// ---------------------------------------------------------------
+// import java.util.*;
+// public class loops {
+//     public static void main(String[] args) {
+//         int n,i,result=0;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the number : ");
+//         n = ob.nextInt();
+
+//         if(n>0){
+//             result = (n*n);
+//             System.out.println(result);
+//         }
+//         else{
+//             result = (n*n);
+//             System.out.println(result);
+//         }
+//     }
+// }
+
+
+// ---------------------------------------------------------------
+//     kisi bhi no ka cubes print karo
+// ---------------------------------------------------------------
+// import java.util.*;
+// public class loops {
+//     public static void main(String[] args) {
+//         int n,i,result=0;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the number : ");
+//         n = ob.nextInt();
+
+//         if(n>0){
+//             result = (n*n*n);
+//             System.out.println(result);
+//         }
+//         else{
+//             result = (n*n*n);
+//             System.out.println(result);
+//         }
+//     }
+// }
+
+
+// ---------------------------------------------------------------
+//     even number ko count karo
+// ---------------------------------------------------------------
+// import java.util.*;
+// public class loops {
+//     public static void main(String[] args) {
+//         int n,i,count=0;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the number : ");
+//         n = ob.nextInt();
+
+//         for(i=1;i<=n;i++){
+//             if(i%2==0){
+//                count = count + 1;
+//             }
+//         }
+//         System.out.print("Count even number : " +count);
+//     }
+// }
+
+// ---------------------------------------------------------------
+//     odd number ko count karo
+// ---------------------------------------------------------------
+// import java.util.*;
+// public class loops {
+//     public static void main(String[] args) {
+//         int n,i,count=0;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the number : ");
+//         n = ob.nextInt();
+
+//         for(i=1;i<=n;i++){
+//             if(i%2!=0){
+//                 count += 1;
+//             }
+//         }
+//         System.out.print("Count odd number : " +count);
+//     }
+// }
+
+
+// ---------------------------------------------------------------
+//     number ko reverse  karo
+// ---------------------------------------------------------------
+// import java.util.*;
+// public class loops {
+//     public static void main(String[] args) {
+//         int n,i,z,rev=0;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the number : ");
+//         n = ob.nextInt();
+
+//         while (n>0) {
+//             z = n%10;
+//             rev = (rev*10)+z;
+//             n = n/10;
+//         }
+//         System.out.print("Reverse no : " +rev);
+//     }
+// }
+
+
+// ---------------------------------------------------------------
+//     chech karo ki palindrom no hai ya nahi
+// ---------------------------------------------------------------
+// import java.util.*;
+// public class loops {
+//     public static void main(String[] args) {
+//         int n,i,z,rev=0;
+//         Scanner ob = new Scanner(System.in);
+//         System.out.print("Enter the number : ");
+//         n = ob.nextInt();
+//         int temp = n;
+//         while (temp>0) {
+//             z = temp%10;
+//             rev = (rev*10)+z;
+//             temp = temp/10;
+//         }
+//        if(rev == n){
+//         System.out.print("Palindrom");
+//        }
+//        else{
+//         System.out.print("Not palindrom");
+//        }
+//     }
+// }
+
+
+
+
